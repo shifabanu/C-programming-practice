c1019de7fec61c965db4d3a538f8edc91b17ad97
Shifa Shaikh <70238196+shifabanu@users.noreply.github.com>
2021-08-22 18:15:46 -0700

    Create check_signs.c

---
+++ bitwise/check_signs.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+Check if the numbers have opposite signs
+HINT: XOR or EXOR
+
+0 0 = 0
+0 1 = 1
+1 0 = 1
+1 1 = 0
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int a, b;
+    bool sign;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    
+    sign =  (a ^ b) < 0;
+    if(sign==true)
+    {
+        printf("Both have opposite signs");
+    }
+    else
+    {
+        printf("Both have same signs");
+    }
+    
+
+
+    return 0;
+}
