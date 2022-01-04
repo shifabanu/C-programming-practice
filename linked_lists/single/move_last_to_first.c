996a43d33a782a5059a975042e298eeb5f937b9c
Shifa Shaikh <70238196+shifabanu@users.noreply.github.com>
2022-01-04 14:07:07 -0800

    Create move_last_to_first.c

---
+++ linked_lists/single/move_last_to_first.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Write a code to move the last node of a single linked list to the first node
+of that list
+
+
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
